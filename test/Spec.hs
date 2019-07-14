b9421bcd0a273019f72ebad8f4e9a4d974e68b87
spudro228 <mompizzapls@mail.ru>
2019-07-14 16:12:07 +0300

    Test + implementation for choice Test for anyOf

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,13 +7,23 @@ main =
     describe "Result data typet" $ do
       it "Success 'a' equal Success 'a' " $ do Success "a" `shouldBe` Success "a"
       it "Success ('a', 'bc') equal Success ('a', 'bc')" $ do Success ("a", "bc") `shouldBe` Success ("a", "bc")
-    describe "\"Or\" [<||>] combinator" $ do
+    describe "\"orElse\" [<||>] combinator" $ do
       it "use first parser" $ do (run (parseChar 'a' `orElse` parseChar 'b') "abc") `shouldBe` Success ("a", "bc")
       it "use alternative parser" $ do (run (parseChar 'a' <||> parseChar 'b') "bca") `shouldBe` Success ("b", "ca")
-    describe "\"And\' [.>>.] combinator" $ do
+    describe "\"andThen\' [.>>.] combinator" $ do
       it "use both parser" $ do (run (parseChar 'a' .>>. parseChar 'b') "abc") `shouldBe` Success (("a", "b"), "c")
     describe "Parser functor [|>>]" $ do
       it "apply sume function for prsing result, use \"fmap\"" $ do
         (run (fmap (++ "a") (parseChar 'a')) "abc") `shouldBe` Success ("aa", "bc")
       it "apply sume function for prsing result, use [|>>]" $ do
         (run ((parseChar 'a') |>> (++ "a")) "abc") `shouldBe` Success ("aa", "bc")
+    describe "\"choice\" choice parser from list of parsers" $ do
+      it "choice last parser" $ do
+        (run (choice [parseChar 'b', parseChar 'c', parseChar 'a']) "abc") `shouldBe` Success ("a", "bc")
+      it "choice first parser" $ do
+        (run (choice [parseChar 'a', parseChar 'c', parseChar 'b']) "abc") `shouldBe` Success ("a", "bc")
+    describe "\"anyOf\" parse any character from list in string" $ do
+      it "choice ['a','f','m']" $ do (run (anyOf ['a', 'm', 'f']) "abc") `shouldBe` Success ("a", "bc")
+      it "choice ['a', 'b', 'c']" $ do (run (anyOf ['a', 'b', 'c']) "abc") `shouldBe` Success ("a", "bc")
+      it "choice number ['0' .. '9']" $ do (run (anyOf ['0' .. '9']) "9abc") `shouldBe` Success ("9", "bc")
+      it "dont choice number ['0' .. '9']" $ do (run (anyOf ['0' .. '9']) "|bc") `shouldBe` Failure ("asdasd")
