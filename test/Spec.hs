778ae8748539ecdf9f63b707be7ec439a1e15d3d
spudro228 <mompizzapls@mail.ru>
2019-07-14 14:36:40 +0300

    Tests for "or"  and "and" combinators

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,14 @@
-import Combinator
-import Test.Hspec
+import           Combinator
+import           Test.Hspec
 
 main :: IO ()
 main =
   hspec $ do
     describe "Result data typet" $ do
-      it "Success 'a' equal Success 'a' " $ do
-        Success "a" `shouldBe` Success "a"
-      it "Success ('a', 'bc') equal Success ('a', 'bc')" $ do
-        Success ("a", "bc") `shouldBe` Success ("a", "bc")
+      it "Success 'a' equal Success 'a' " $ do Success "a" `shouldBe` Success "a"
+      it "Success ('a', 'bc') equal Success ('a', 'bc')" $ do Success ("a", "bc") `shouldBe` Success ("a", "bc")
+    describe "\"Or\" [<||>] combinator" $ do
+      it "use first parser" $ do (run (parseChar 'a' `orElse` parseChar 'b') "abc") `shouldBe` Success ("a", "bc")
+      it "use alternative parser" $ do (run (parseChar 'a' <||> parseChar 'b') "bca") `shouldBe` Success ("b", "ca")
+    describe "\"And\' [.>>.] combinator" $ do
+      it "use both parser" $ do (run (parseChar 'a' .>>. parseChar 'b') "abc") `shouldBe` Success (("a", "b"), "c")
